b78e23b98f7350d3f9b6ca4e4aeae5a8a7080d1c
zxc0328 <120797887@qq.com>
2017-02-19 20:40:28 +0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 'use strict'
 
-var YAJB = function(options){
+var YAJB = function(){
+	var options
+	// get global options
+	if (window.javaInterface) {
+		options = JSON.parse(window.javaInterface())
+	}else if (window.YAJB_INJECT){
+		options = window.YAJB_INJECT
+	}else {
+		throw new Error('No Global Inject Object detected, please use YAJB-js in a YAJB WebView Environment.');
+	}
 	this.platform = options.platform
 	this.data = options.data
+}
+
+YAJB.prototype.isMobile = function() {
+	if (window.javaInterface || window.YAJB_INJECT) {
+		return true
+	}else {
+		return false
+	}
 }
 
 YAJB.prototype.send = function() {
