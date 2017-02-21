f8aa065b8ea8383671a658ce08945ddb517a248e
zxc0328 <120797887@qq.com>
2017-02-21 21:15:34 +0800

    add event module

--- event.js
+++ event.js
@@ -1,13 +1,46 @@
 'use strict'
 
-var Event = function(){}
-
-Event.prototype.on = function () {
-
+var Event = function() {
+  this.listeners = {};
 }
 
-Event.prototype.trigger = function () {
+Event.prototype.on = function(event, fn) {
+  event = '$' + event;
+  (listeners[event] || (listeners[event] = [])).push(fn)
+}
 
+Event.prototype.emit: function(event) {
+  event = '$' + event
+  let cbs = listeners[event]
+  if (cbs) {
+    const args = [].slice.call(arguments, 1)
+    cbs = cbs.slice()
+    for (let i = 0, l = cbs.length; i < l; i++) {
+      cbs[i].apply(this, args)
+    }
+  }
+}
+
+Event.prototype.off: function(event, fn) {
+  event = '$' + event
+  if (!arguments.length) {
+    listeners = {}
+  } else {
+    const cbs = listeners[event]
+    if (cbs) {
+      if (!fn) {
+        listeners[event] = null
+      } else {
+        for (let i = 0, l = cbs.length; i < l; i++) {
+          const cb = cbs[i]
+          if (cb === fn || cb.fn === fn) {
+            cbs.splice(i, 1)
+            break
+          }
+        }
+      }
+    }
+  }
 }
 
 export default Event
